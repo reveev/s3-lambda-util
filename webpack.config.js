9d6936f2bd54b9f22ea23d201a57af65e79131e8
apylon <dev.apylon@gmail.com>
2020-05-04 16:01:54 +0530

    Change library name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    library: 'reveev-s3-lambda-util',
+    library: 'S3LambdaUtil',
     libraryTarget: 'commonjs2'
   },
   target: 'node',
